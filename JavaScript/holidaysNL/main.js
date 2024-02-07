f0bf1cf13c98235ff6c8f288753f6cdcfb6ed60f
pschilling28 <schilling.pat@gmail.com>
2024-02-07 16:02:57 +0100

    started holidayNL problem

---
+++ JavaScript/holidaysNL/main.js
@@ -0,0 +1,11 @@
+/* rules
+    - there are two types of holidays, legal and personl, each with different expiration lengths (ex leg 5yrs, pers 3yrs)
+    - 24 legal holidays award per year, 6 personal
+    - 10 holidays can be carried over, but they need to be earmarked which kind of holidays (ex 6 leg 4 pers)
+    - there is a priority to which holidays are used first in a holiday bank (oldest holidays used first)
+    - there is a priority which holidays are carried over first to the next year (ex must carry over all leg before any pers)
+*/
+
+let holiday = () => {
+    
+}
